87d35116eb4ec6f224ccf3440df001ff4a3b8aaa
shilei.yu <shilei.yu@zhaopin.com.cn>
2018-03-29 11:14:37 +0800

    1：测试MaterialCalendar selectRange

--- app/src/main/java/com/shileiyu/baseapp/ui/calendar/CalendarActivity.java
+++ app/src/main/java/com/shileiyu/baseapp/ui/calendar/CalendarActivity.java
@@ -3,11 +3,17 @@ package com.shileiyu.baseapp.ui.calendar;
 
 import android.support.annotation.NonNull;
 
+import com.orhanobut.logger.Logger;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
 import com.shileiyu.baseapp.R;
 import com.shileiyu.baseapp.common.base.BaseActivity;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -23,12 +29,37 @@ public class CalendarActivity extends BaseActivity {
         return R.layout.activity_calendar;
     }
 
+    private CalendarDay first;
+    private CalendarDay second;
+
     @Override
     protected void initView() {
         mCalendar.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                if (selected) {
+                    if (first == null) {
+                        first = date;
+                    } else {
+                        if (first.isBefore(date)) {
+                            second = date;
+                        } else {
+                            first = date;
+                            second = null;
+                        }
+                    }
+                }
+                if (first != null && second != null) {
+                    mCalendar.selectRange(first, second);
+                }
                 showToast(selected + " " + date.toString());
+                Logger.d("onDateSelected = " + date);
+            }
+        });
+        mCalendar.setOnRangeSelectedListener(new OnRangeSelectedListener() {
+            @Override
+            public void onRangeSelected(@NonNull MaterialCalendarView widget, @NonNull List<CalendarDay> dates) {
+                Logger.d("onRangeSelected = " + dates);
             }
         });
     }
